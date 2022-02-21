adc3e003be758fcad82342c4444a30095b4b4854
Trouni Tiet <trouni@gmail.com>
2022-02-21 10:08:00 +0900

    installed devise into the app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+  end
+
+  def about
+    # Requires login unless we add it in the skip_before_action
   end
 end
